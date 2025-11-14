ef19f067158766b318213f8fa811f4cba86b70ac
chrisw <chrispersonalemail123@gmail.com>
2025-11-13 20:33:57 -0800

    small reorganization

--- day5.py
+++ day5.py
@@ -7,7 +7,12 @@ strings = open('day5.txt', 'r').read().splitlines()
 GOOD_STRINGS = 0
 
 for line in strings:
-    if (len(re.findall(r'[aeiou]', line)) >= 3) and (len(re.findall(r'(\w)\1', line)) >= 1) and (len(re.findall(r'(ab|cd|pq|xy)', line)) == 0):
+
+    vowels = re.findall(r'[aeiou]', line)
+    doubles = re.findall(r'(\w)\1', line)
+    bads = re.findall(r'(ab|cd|pq|xy)', line)
+
+    if (len(vowels) >= 3) and (len(doubles) >= 1) and (len(bads) == 0):
         GOOD_STRINGS += 1
 
 print(f'PART 1: There are {GOOD_STRINGS} nice strings in the input.')
@@ -17,8 +22,10 @@ print(f'PART 1: There are {GOOD_STRINGS} nice strings in the input.')
 NICE_STRINGS = 0
 
 for line in strings:
+
     pairs = re.findall(r'(?P<f>\w\w).*(?P=f)', line)
     ones = re.findall(r'(?P<s>\w).(?P=s)', line)
+    
     if (len(pairs) >= 1) and (len(ones) >= 1):
         NICE_STRINGS += 1
 
